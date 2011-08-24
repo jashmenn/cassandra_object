a62d59cb76f09de845d56f0ebca998fe269c4742
Nate Murray <nate@natemurray.com>
2011-08-24 08:02:57 -0700

    uncommented this test

--- test/validation_test.rb
+++ test/validation_test.rb
@@ -5,20 +5,19 @@ class ValidationTest < CassandraObjectTestCase
   test "save! raises an error" do
     begin
       customer = Customer.new :first_name=>"steve", :date_of_birth=>Date.parse("1979/12/25")
-      customer.valid?
-      #customer.save!
-      #flunk "Should have failed to save"
+      customer.save!
+      flunk "Should have failed to save"
     rescue CassandraObject::Validation::RecordInvalidError => e
-      # assert_equal customer, e.record
+      assert_equal customer, e.record
     end
   end
 
   test "create! raises an error" do
     begin
-      #customer = Customer.create! :first_name=>"steve", :date_of_birth=>Date.parse("1979/12/25")
-      #flunk "Should have failed to create!"
+      customer = Customer.create! :first_name=>"steve", :date_of_birth=>Date.parse("1979/12/25")
+      flunk "Should have failed to create!"
     rescue CassandraObject::Validation::RecordInvalidError => e
-      # assert_kind_of Customer, e.record
+      assert_kind_of Customer, e.record
     end
   end
 
